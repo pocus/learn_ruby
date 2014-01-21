143940f178bbbedca713b37a4d5f8073bbba195a
David Yip <dvd.yip@gmail.com>
2014-01-21 16:37:56 -0500

    all works

---
+++ 02_calculator/calculator.rb
@@ -0,0 +1,28 @@
+#David Yip
+
+def add(num1,num2)
+	num1 + num2
+end
+
+def subtract(num1,num2)
+	num1 - num2
+end
+
+def sum(ary)
+
+	if ary == [] then return 0 end
+
+	result = 0
+	ary.each do |n|
+
+		result = result + n
+
+	end
+
+	result
+
+
+
+end
+
+
