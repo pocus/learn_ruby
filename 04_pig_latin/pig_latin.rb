c869535c737a0cacc9ba306f45f8f966d7b939cf
David Yip <dvd.yip@gmail.com>
2014-01-21 16:30:04 -0500

    9 examples 0 failures

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -2,47 +2,33 @@
 
 def translate(word)
 
-	phonemes = ["sch","qu"]
+	if word.include?(" ")
 
-	assembled_word(word,number_of_leading_cons(word))
+		sub_words = word.split(" ")
+		result = []
 
-#	text_ary = text.split(" ")
-#	titled_ary = []
-#	titled_ary  = text_ary.collect do |word|
+		sub_words.each do |w|
+			result << assembled_word(w,number_of_leading_cons(w))
+		end
 
-	#if (phonemes.include?(word[0,2]) || phonemes.include?(word[0,3])) #for sch and qu
+		result.join(" ")
 
-	#erry.ch.ay
-
-
-
-			# else
-			# 	word[1..word.length] + word[0] + "ay" #words beginning with one consonant
-			# end
-
-
-	#end
-
-
-	# if consonants.include?(word[0]) && consonants.include?(word[1])  #&& consonants.include?(word[2])
-
-	# word[2..word.length] + word[0] + word[1] + "ay" # two initial constants
-
-	# end
-
+		else
+			assembled_word(word,number_of_leading_cons(word))
+		end
 end
 
 def number_of_leading_cons(word)
 	consonants = ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z"]
-	num = 0
 
 	if word[0] + word[1] == "qu"
 	 return 2
 
 	elsif (word.include?("qu") && word[0] + word[1] != "qu")
 		word.index("u") + 1
-	else
 
+	else
+		num = 0
 		word.each_char do |c|
 
 			if 	consonants.include?(c)
