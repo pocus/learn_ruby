e876fcdaae1cfd9b40251f04f2d0ad0304f6eda6
David Yip <dvd.yip@gmail.com>
2014-01-22 14:28:14 -0500

    calling new inside a class method

--- 10_temperature_object/temperature.rb
+++ 10_temperature_object/temperature.rb
@@ -1,40 +1,46 @@
 class Temperature
 
-
-
 	def initialize(hash)
 		@temp_hash = hash
+		@value = nil
+		@unit = nil
+
+		if @temp_hash.has_key?(:f)
+			@unit = :f
+			@value = @temp_hash[:f].to_i
+
+		elsif @temp_hash.has_key?(:c)
+			@unit = :c
+			@value = @temp_hash[:c].to_i
+		end
+
 	end
 
 	def to_fahrenheit
-
-		if @temp_hash.has_key?(:f)
-			@temp_hash[:f]
-
-		elsif @temp_hash.has_key?(:c)
-			(@temp_hash[:c].to_i * 9/5.0) + 32
-
+		if @unit == :f
+			@value.ceil
+		elsif @unit == :c
+		((@value * 9/5.0) + 32).ceil
 		end
-
-
 	end
 
+
 	def to_celsius
+		if @unit == :f
+			 ((@value -32) * 5/9.0).ceil
 
-		if @temp_hash.has_key?(:f)
-			(@temp_hash[:f] -32) * 5/9.0
-
-		elsif @temp_hash.has_key?(:c)
-			@temp_hash[:c]
-
+		elsif @unit == :c
+			@value.ceil
 		end
-
-
 	end
 
 
 	def self.in_celsius(cel)
+		new(:c => cel) #calling new inside a Class method ... novel.
 	end
 
+	def self.in_fahrenheit(fah)
+		new(:f => fah)
+	end
 
 end
